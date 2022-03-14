a216699d131039e3cb38cdff2e5fb11b507f93e0
Somdotta07 <jhilick.s@gmail.com>
2022-03-14 14:25:20 +0100

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :groups, dependent: :destroy
   has_many :expenses, dependent: :destroy
+
+  validates :name, :email, presence: true, length: { maximum: 255 }
+  validates :email, uniqueness: true
 end
